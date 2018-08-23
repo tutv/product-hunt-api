d149cbe1832a6ac23901baed298fb3b6c7ab7280
Max <tutv95@gmail.com>
2018-08-23 23:53:29 +0700

    Updated

--- src/app.database.js
+++ src/app.database.js
@@ -2,10 +2,6 @@ const createConnection = require('./libs/createConnection')
 const getEnv = require("./helpers/getEnv")
 const uri = getEnv('/mongodb')
 
-console.log(uri)
-
-const app = createConnection(uri, {
-    //poolSize: 10
-})
+const app = createConnection(uri)
 
 module.exports = app
